1aa9c440a799d602e88e3dccad80495209b63fc1
RDI <dkdlsekdls@naver.com>
2023-01-25 21:32:44 +0900

    feat/Delete Nweet

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,12 +1,21 @@
 import React from "react";
+import { dbService } from "fBase";
 
 const Nweet = ({ nweetObj, isOwner }) => {
+  const onDeleteClick = async () => {
+    const ok = window.confirm("Are you sure you want to delete this nweet?");
+    console.log(ok);
+    if (ok) {
+      //delete nweet
+      await dbService.doc(`nweets/${nweetObj.id}`).delete();
+    }
+  };
   return (
     <div>
       <h4>{nweetObj.text}</h4>
       {isOwner && (
         <>
-          <button>Delete Nweet</button>
+          <button onClick={onDeleteClick}>Delete Nweet</button>
           <button>Edit Nweet</button>
         </>
       )}
